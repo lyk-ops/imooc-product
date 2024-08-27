0d68fe5db80a695801b33dd9ec6222187c97615c
Administrator <1284637687@qq.com>
2024-08-27 21:16:54 +0800

    前端调整

--- backend/main.go
+++ backend/main.go
@@ -10,7 +10,6 @@ import (
 	"imooc-product/common"
 	"imooc-product/repositories"
 	"imooc-product/services"
-	"log"
 )
 
 func main() {
@@ -19,32 +18,31 @@ func main() {
 	//2.设置错误模式，在mvc模式下提示错误
 	app.Logger().SetLevel("debug")
 	//3.注册模板
-	tmplate := iris.HTML("./backend/web/views", ".html").Layout("shared/layout.html").Reload(true)
-	app.RegisterView(tmplate)
+	//template := iris.HTML("./backend/web/views", ".html")
+	template := iris.HTML("./backend/web/views", ".html").
+		Layout("shared/layout.html").
+		Reload(true)
+	//template := iris.HTML("./backend/web/views", ".html").Layout("shared/layout.html").Reload(true)
+	app.RegisterView(template)
 	//4.设置模板目标
-	app.HandleDir("/assets", "./backend/web/assets", iris.DirOptions{
-		// 这里可以添加一些选项，但如果你不需要特别配置，可以省略这些选项
-		// 例如，启用或禁用浏览静态目录（ListDirectory: true/false）
-		// 自定义HTTP头（Headers: map[string]string{...}）
-		// 启用或禁用索引文件（IndexName: "index.html"）等
-	})
+	app.HandleDir("/assets", "./backend/web/assets")
 
 	//http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./backend/web/assets"))))
 	//出现异常跳转到指定页面
 
 	app.OnAnyErrorCode(func(ctx iris.Context) {
-		log.Println("Before calling potentially problematic code...")
+		fmt.Printf("code:%d", ctx.GetStatusCode())
+		fmt.Println()
+		logrus.Println("Before calling potentially problematic code...")
 		ctx.ViewData("message", ctx.Values().GetStringDefault("message", "访问的页面出错！"))
 		ctx.ViewLayout("")
+		fmt.Println("调用shared/error.html")
 		err := ctx.View("shared/error.html")
 		if err != nil {
-			fmt.Println(err)
-			log.Println("After calling potentially problematic code...")
-			return
+			logrus.Error(err)
 		}
-		log.Println("After calling potentially problematic code...")
+		logrus.Println("After calling potentially problematic code...")
 	})
-	// 第 32 行及其周围的代码
 
 	//连接数据库
 	db, err := common.NewMysqlConn()
@@ -61,9 +59,12 @@ func main() {
 	product.Register(ctx, productSerivce)
 	product.Handle(new(controllers.ProductController))
 	//启动服务
-	app.Run(iris.Addr(":8080"),
+	err = app.Run(iris.Addr(":8080"),
 		iris.WithoutServerError(iris.ErrServerClosed),
 		iris.WithOptimizations,
 	)
+	if err != nil {
+		return
+	}
 
 }
